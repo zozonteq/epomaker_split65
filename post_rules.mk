07a4ce7f486ec201aeb005abcc098688128ead94
zozonteq <ottotak@outlook.com>
2026-01-02 00:43:48 +0900

    fix: fix build issue

--- post_rules.mk
+++ post_rules.mk
@@ -1,4 +1,4 @@
 RGB_MATRIX_CUSTOM_USER = yes
 include keyboards/leo/epomaker_split65/rgb_record/rgb_record.mk
 include keyboards/leo/epomaker_split65/wls/wls.mk
-include keyboards/linker/wireless/wireless.mk
+include keyboards/leo/epomaker_split65/linker/wireless/wireless.mk
