b08463c264ab54730e199d3f084bd3387a1b799d
zozonteq <ottotak@outlook.com>
2026-01-03 00:01:31 +0900

    add: Add gamemode layer

--- keymaps/my-layout/keymap.c
+++ keymaps/my-layout/keymap.c
@@ -9,6 +9,8 @@ enum layers {
     _FL,
     _MBL,
     _MFL,
+    _GBL, // gamemode
+    _GFL
 };
 
 #define ______ HS_BLACK
@@ -23,33 +25,46 @@ bool is_d_pressed = false;
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BL] = LAYOUT( /* Base */
+    [_BL] = LAYOUT( /* Base Layer */
         KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                 KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, KC_MUTE,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                 KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS, KC_DEL,
-        MO(_FL),  QMK_A,    KC_S,     QMK_D,    KC_F,     KC_G,                 KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,            KC_PGUP,
+        MO(_FL),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                 KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,            KC_PGUP,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                 KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,   KC_PGDN,
         KC_LCTL,  KC_LCMD,  KC_LALT,  KC_SPC,                                   KC_SPC,   KC_RALT,  MO(_FL),  KC_RCTL,                      KC_LEFT,  KC_DOWN, KC_RGHT),
 
-    [_FL] = LAYOUT( /* Base */
+    [_FL] = LAYOUT( /* Base Function Layer*/
         KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   EE_CLR,  _______,
         RGB_MOD,  KC_BT1,   KC_BT2,   KC_BT3,   KC_BT4,   KC_BT5,               _______,  _______,  _______,  _______,  _______,  RGB_HUD,  RGB_HUI,  _______, KC_INS,
-        _______,  TO(_BL),  KC_S,     _______,  _______,  _______,              KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  RGB_SAD,  RGB_SAI,  _______,           KC_HOME,
-        _______,  _______,  RGB_TOG,  _______,  _______,  _______,              NK_TOGG,  _______,  _______,  _______,  _______,            _______,  RGB_VAI, KC_END,
-        KC_FILP,  GU_TOGG,  _______,  KC_BATQ,                                  KC_BATQ,  _______,  _______,  _______,                      RGB_SPD,  RGB_VAD, RGB_SPI),
+        _______,  TO(_BL),  KC_S,     _______,  _______,  TO(_GBL),             KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  RGB_SAD,  RGB_SAI,  _______,           KC_HOME,
+        KC_LSFT,  _______,  RGB_TOG,  _______,  _______,  _______,              NK_TOGG,  TO(_MBL),  _______,  _______,  _______,            _______,  RGB_VAI, KC_END,
+        KC_LCTL,  GU_TOGG,  _______,  KC_BATQ,                                  KC_BATQ,  _______,  _______,  _______,                      RGB_SPD,  RGB_VAD, RGB_SPI),
 
-    [_MBL] = LAYOUT( /* Base */
+    [_MBL] = LAYOUT( /* Mac base layer*/
         KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                 KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, KC_MUTE,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                 KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS, KC_DEL,
-        KC_LCTL,  QMK_A,    KC_S,     QMK_D,    KC_F,     KC_G,                 KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,            KC_PGUP,
+        KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                 KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,            KC_PGUP,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                 KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,   KC_PGDN,
         KC_LCTL,  KC_LALT,  KC_LGUI,  KC_SPC,                                   KC_SPC,   KC_RGUI,  MO(_MFL), KC_RCTL,                      KC_LEFT,  KC_DOWN, KC_RGHT),
 
-    [_MFL] = LAYOUT( /* Base */
+    [_MFL] = LAYOUT( /* Mac function layer */
         KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   EE_CLR,  _______,
         RGB_MOD,  KC_BT1,   KC_BT2,   KC_BT3,   KC_BT4,   KC_BT5,               _______,  _______,  _______,  _______,  _______,  RGB_HUD,  RGB_HUI,  _______, KC_INS,
-        _______,  TO(_BL),  KC_S,     _______,  _______,  _______,              KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  RGB_SAD,  RGB_SAI,  _______,           KC_HOME,
-        _______,  _______,  RGB_TOG,  _______,  _______,  _______,              NK_TOGG,  _______,  _______,  _______,  _______,            _______,  RGB_VAI, KC_END,
-        KC_FILP,  _______,  _______,  KC_BATQ,                                  KC_BATQ,  _______,  _______,  _______,                      RGB_SPD,  RGB_VAD, RGB_SPI),
+        _______,  TO(_BL),  KC_S,     _______,  _______,  TO(_GBL),             KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  RGB_SAD,  RGB_SAI,  _______,           KC_HOME,
+        KC_LSFT,  _______,  RGB_TOG,  _______,  _______,  _______,              NK_TOGG,  TO(_BL),  _______,  _______,  _______,            _______,  RGB_VAI, KC_END,
+        KC_LCTL,  _______,  _______,  KC_BATQ,                                  KC_BATQ,  _______,  _______,  _______,                      RGB_SPD,  RGB_VAD, RGB_SPI),
+    [_GBL] = LAYOUT( /* Game base layer (QWERTY)*/
+        KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                 KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, KC_MUTE,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                 KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS, KC_DEL,
+        _______,  QMK_A,    KC_S,     QMK_D,    KC_F,     KC_G,                 KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,            KC_PGUP,
+        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                 KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,   KC_PGDN,
+        KC_LCTL,  KC_LCMD,  KC_LALT,  KC_SPC,                                   KC_SPC,   KC_RALT,  MO(_GFL),  KC_RCTL,                      KC_LEFT,  KC_DOWN, KC_RGHT),
+
+    [_GFL] = LAYOUT( /* Game function layer (QWERTY)*/
+        KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   EE_CLR,  _______,
+        RGB_MOD,  KC_BT1,   KC_BT2,   KC_BT3,   KC_BT4,   KC_BT5,               _______,  _______,  _______,  _______,  _______,  RGB_HUD,  RGB_HUI,  _______, KC_INS,
+        _______,  TO(_BL),  KC_S,     _______,  _______,  TO(_GBL),             KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  RGB_SAD,  RGB_SAI,  _______,           KC_HOME,
+        KC_LSFT,  _______,  RGB_TOG,  _______,  _______,  _______,              NK_TOGG,  _______,  _______,  _______,  _______,            _______,  RGB_VAI, KC_END,
+        KC_LCTL,  GU_TOGG,  _______,  KC_BATQ,                                  KC_BATQ,  _______,  _______,  _______,                      RGB_SPD,  RGB_VAD, RGB_SPI),
 
 };
 
